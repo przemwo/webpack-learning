904fc4938d4f67a2483d5a8d05cfaf8faf1ac0f7
przemwo <przemwo@o2.pl>
2016-10-06 11:31:31 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// TODO: Add .giignore!!!
+
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // make .html file
+const merge = require('webpack-merge'); // merge objects into one
+const validate = require('webpack-validator'); // watch if webpack config is ok
+
+const parts = require('./libs/parts');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+};
+
+var config;
+
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common, {});
+    break;
+  default:
+    config = merge(
+      common,
+      parts.devServer({
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
+}
+
+module.exports = validate(config);
